5ad02f95e190f66c57b9bc9121fe4562ce696f28
Brendan Fahy <brendan@f4hy.com>
2013-04-03 18:14:03 -0400

    switch to shared rather than firstprivate to fix large memory issies

--- openmp.cpp
+++ openmp.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
             blocks[x_index + y_index*blocksize][number_in_block[x_index + y_index*blocksize]-1] = particles+p;
         }
 
-#pragma omp parallel for shared(blocks) firstprivate(number_in_block) reduction (+:navg) reduction(+:davg)
+#pragma omp parallel for shared(blocks) shared(number_in_block) reduction (+:navg) reduction(+:davg)
         for(int i=0; i<blocksize; i++){
             for(int j=0; j<blocksize; j++){
                 for(int p=0; p<number_in_block[i + j*blocksize]; p++ ){
